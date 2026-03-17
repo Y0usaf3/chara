3b96c77016d6b46210d3e80065b5e50adaac154a
Youssef Lmouden <youssef.pro.coding@gmail.com>
2026-03-17 14:01:07 +0000

    aaaa

--- flake.nix
+++ flake.nix
@@ -3,15 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
   };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      naersk,
+    }:
     let
       pkgs = nixpkgs.legacyPackages."x86_64-linux";
     in
     {
-      packages."x86_64-linux".default = pkgs.callPackage ./default.nix { };
+      # packages."x86_64-linux".default = pkgs.callPackage ./default.nix { };
+      packages."x86_64-linux".default = naerskLib.buildPackage {
+        srs = ./.;
+        nativeBuildInputs = [ pkgs.pkg-config ];
+      };
 
       devShells."x86_64-linux".default = pkgs.mkShell {
         buildInputs = with pkgs; [
